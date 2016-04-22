e3e1ceaaf8dbf3bce501d13309c22af63d39a5fe
Alex Clay <alex@suran.com>
2016-04-21 21:19:12 -0400

    Fix script

--- script/minimize.sh
+++ script/minimize.sh
@@ -22,9 +22,9 @@ echo Zeroing out free space
 diskutil secureErase freespace 0 ${slash}
 
 # VMware Fusion specific items
-if [ -e .vmfusion_version ] || [[ "$PACKER_BUILDER_TYPE" == vmware* ]]; then
-    # Shrink the disk
-    # /Library/Application\ Support/VMware\ Tools/vmware-tools-cli disk shrink /
-fi
+# if [ -e .vmfusion_version ] || [[ "$PACKER_BUILDER_TYPE" == vmware* ]]; then
+#     # Shrink the disk
+#     /Library/Application\ Support/VMware\ Tools/vmware-tools-cli disk shrink /
+# fi
 
 echo "Minimize finished"
