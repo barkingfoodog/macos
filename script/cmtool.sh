37df55a590cebd25f51f4e3e3a07967353a99aa9
Alex Clay <alex@suran.com>
2016-11-17 14:16:19 -0500

    Tweaking the salt install

--- script/cmtool.sh
+++ script/cmtool.sh
@@ -47,9 +47,9 @@ install_salt()
     echo "==> Installing Salt provisioner"
 
     echo "-- Installing Homebrew"
-    mkdir -p "/usr/local" 
-    curl -L --silent "https://github.com/Homebrew/brew/tarball/master" | tar zx - --strip 1 -C "/usr/local" 
+    mkdir -p /usr/local
     chown -R "${SSH_USERNAME}" /usr/local
+    su - "${SSH_USERNAME}" -c "curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /usr/local"
  
     echo "-- Adding Homebrew to ${SSH_USERNAME} user's PATH"
     su - "${SSH_USERNAME}" -c "echo 'export PATH=/usr/local/bin:/usr/local/sbin:\$PATH' >> .bash_profile && chmod 0700 .bash_profile"
